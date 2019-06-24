7e8559886e0352608b39e7e436b248335b322721
Hasibur Rahman Omi <hrahmanomi@gmail.com>
2019-06-24 13:22:50 +0600

    Ordered date added in list table

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -10,7 +10,7 @@
     <div class="row">
         <div class="col-md-12">
             <div class="table-responsive">
-                <table class="table table-bordered table-hover" id="customers-table">
+                <table class="table table-bordered table-hover" id="orders-table">
                     <thead>
                     <tr>
                         <th>#</th>
@@ -18,21 +18,23 @@
                         <th>Customer</th>
                         <th>Total Amount</th>
                         <th>Status</th>
+                        <th>Date</th>
                         <th></th>
                     </tr>
                     </thead>
 
                     <tbody>
-                    @foreach ($orders as $i => $order)
-                        <tr>
-                            <td>{{ $i + 1 }}</td>
-                            <td>{{ $order->invoice_number }}</td>
-                            <td>{{ $order->customer->name }}</td>
-                            <td>£ {{ $order->total_amount }}</td>
-                            <td>{{ $order->status }}</td>
-                            <td><a href="{{ route('orders.details', $order->invoice_number) }}" target="_blank">View</a></td>
-                        </tr>
-                    @endforeach
+                        @foreach ($orders as $i => $order)
+                            <tr>
+                                <td>{{ $i + 1 }}</td>
+                                <td>{{ $order->invoice_number }}</td>
+                                <td>{{ $order->customer->name }}</td>
+                                <td>£ {{ $order->total_amount }}</td>
+                                <td>{{ $order->status }}</td>
+                                <td>{{ $order->created_at->format('m/d/Y') }}</td>
+                                <td><a href="{{ route('orders.details', $order->invoice_number) }}" target="_blank">View</a></td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
@@ -45,7 +47,7 @@
     <script src="{{ asset('vendor/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
     <script>
         $(document).ready(function() {
-            $('#customers-table').DataTable();
+            $('#orders-table').DataTable();
         });
     </script>
 @endsection
